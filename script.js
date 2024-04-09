cfbda4abfc13c7cc4f81efebe726b6797d1a4eb0
Katelyn Nguyen <ktynguyen@ucdavis.edu>
2024-04-09 04:51:40 -0700

    home page updates

---
+++ script.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section');
+    const h3 = document.querySelector('h3');
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            h3.className = 'switch';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            h3.className = ('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
